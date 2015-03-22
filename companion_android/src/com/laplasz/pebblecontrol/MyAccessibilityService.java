0f3b9b3289224ef2bdbdfcf953b30cbf13408df7
laplasz <laplasz@freemail.hu>
2015-03-22 22:30:58 +0100

    service now started if button pressed

--- companion_android/src/com/laplasz/pebblecontrol/MyAccessibilityService.java
+++ companion_android/src/com/laplasz/pebblecontrol/MyAccessibilityService.java
@@ -1,21 +1,15 @@
 package com.laplasz.pebblecontrol;
 
-import java.util.UUID;
-
-import com.getpebble.android.kit.PebbleKit;
-import com.getpebble.android.kit.PebbleKit.PebbleDataReceiver;
-import com.getpebble.android.kit.util.PebbleDictionary;
 
 import android.accessibilityservice.AccessibilityService;
-import android.content.Context;
+import android.app.Service;
+import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 
 public class MyAccessibilityService extends AccessibilityService  {
 
-	private final static UUID PEBBLE_APP_UUID = UUID.fromString("6954047f-48b4-467c-815e-f2fe666b1388");
-	private PebbleDataReceiver mReceiver;
-	
 	private static final int
     KEY_BUTTON_EVENT = 0,
     BUTTON_EVENT_UP = 1,
@@ -25,37 +19,41 @@ public class MyAccessibilityService extends AccessibilityService  {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		
+		Log.d("service", "crated");
+			
+	}
+	
+	@Override
+	public int onStartCommand(Intent intent, int flags, int startId) {
 		Log.d("service", "started");
-		mReceiver = new PebbleDataReceiver(PEBBLE_APP_UUID) {
-			 
-		    @Override
-		    public void receiveData(Context context, int transactionId, PebbleDictionary data) {
-		    	//ACK the message
-		        PebbleKit.sendAckToPebble(context, transactionId);
-		        //Check the key exists
-		        if(data.getUnsignedIntegerAsLong(KEY_BUTTON_EVENT) != null) {
-		            int button = data.getUnsignedIntegerAsLong(KEY_BUTTON_EVENT).intValue();
-		     
-		            switch(button) {
-		            case BUTTON_EVENT_UP:
-		                //The UP button was pressed
-		            	 performGlobalAction(GLOBAL_ACTION_BACK);
-		                break;
-		            case BUTTON_EVENT_DOWN:
-		                //The DOWN button was pressed
-		            	
-		                break;
-		            case BUTTON_EVENT_SELECT:
-		                //The SELECT button was pressed
+		Bundle extras = intent.getExtras();
+		Integer value = -1;
+		if (extras != null) {
+		    value = extras.getInt("control");
+		}
+		
+		switch(value) {
+            case BUTTON_EVENT_UP:
+                //The UP button was pressed
+            	 boolean result = performGlobalAction(GLOBAL_ACTION_BACK);
+            	 Log.d("make back action result: ", Boolean.toString(result));
+                break;
+            case BUTTON_EVENT_DOWN:
+                //The DOWN button was pressed
+            	
+                break;
+            case BUTTON_EVENT_SELECT:
+                //The SELECT button was pressed
 
-		                break;
-		            }
-		        }
-		    }
-
-		};
-
-		PebbleKit.registerReceivedDataHandler(this, mReceiver);		
+                break;
+            default:
+            	Log.d("keyboard","no such");
+            	
+            }
+		stopSelf();
+		return Service.START_STICKY;
+		
 	}
 	
 	@Override
@@ -76,7 +74,6 @@ public class MyAccessibilityService extends AccessibilityService  {
         // Cancel the persistent notification.
 		super.onDestroy();
 		Log.d("service", "destroyed");
-		unregisterReceiver(mReceiver);
         
     }
 
--- companion_android/src/com/laplasz/pebblecontrol/MyBroadCastReceiver.java
+++ companion_android/src/com/laplasz/pebblecontrol/MyBroadCastReceiver.java
@@ -60,19 +60,26 @@ public class MyBroadCastReceiver extends PebbleDataReceiver {
 	            
 	            if(data.getUnsignedIntegerAsLong(KEY_BUTTON_EVENT) != null) {
 	                int button = data.getUnsignedIntegerAsLong(KEY_BUTTON_EVENT).intValue();
+	                Intent service = new Intent(context, MyAccessibilityService.class);
 	         
 	                switch(button) {
 	                case BUTTON_EVENT_UP:
 	                    //The UP button was pressed
 	                	Toast.makeText(context, "UP!", Toast.LENGTH_LONG).show();
+	                	service.putExtra("control", BUTTON_EVENT_UP);
+	            		context.startService(service);
 	                    break;
 	                case BUTTON_EVENT_DOWN:
 	                    //The DOWN button was pressed
 	                	Toast.makeText(context, "DOWN!", Toast.LENGTH_LONG).show();
+	                	service.putExtra("control", BUTTON_EVENT_DOWN);
+	            		context.startService(service);
 	                    break;
 	                case BUTTON_EVENT_SELECT:
 	                    //The SELECT button was pressed
 	                	Toast.makeText(context, "SELECT!", Toast.LENGTH_LONG).show();
+	                	service.putExtra("control", BUTTON_EVENT_SELECT);
+	            		context.startService(service);
 	                    break;
 	                }
 	            }
