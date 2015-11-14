d20f64244da2b206e0847f1491faf6c45eeac8e0
laplasz <laplasz@freemail.hu>
2015-11-14 17:09:02 +0100

    add back button

--- src/button_click.c
+++ src/button_click.c
@@ -7,7 +7,8 @@ enum {
     KEY_BUTTON_EVENT = 0,
     BUTTON_EVENT_UP = 1,
     BUTTON_EVENT_DOWN = 2,
-    BUTTON_EVENT_SELECT = 3
+    BUTTON_EVENT_SELECT = 3,
+    BUTTON_EVENT_BACK = 4
 };
 
 void send_int(uint8_t key, uint8_t cmd)
@@ -37,10 +38,16 @@ static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
   send_int(KEY_BUTTON_EVENT, BUTTON_EVENT_DOWN);
 }
 
+static void back_click_handler(ClickRecognizerRef recognizer, void *context) {
+  text_layer_set_text(text_layer, "Back");
+  send_int(KEY_BUTTON_EVENT, BUTTON_EVENT_BACK);
+}
+
 static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
   window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+  window_single_click_subscribe(BUTTON_ID_BACK, back_click_handler);
 }
 
 static void window_load(Window *window) {
